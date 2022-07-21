c273e99d15ea0ba92d9e9f693bd3e81f9f4527f5
Alfad Sabil Haq <alfadsabilhaq@gmail.com>
2022-07-22 02:33:15 +0700

    Shift

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/hari", require("./hari"));
+router.use("/shift", require("./shift"));
+router.use("/santri", require("./santri"));
 router.use("/controller", require("./detail_reservasi_santri"));
 router.use("/controller", require("./detail_reservasi_wali"));
 router.use("/controller", require("./informasi"));
@@ -11,8 +13,6 @@ router.use("/controller", require("./mahrom"));
 router.use("/controller", require("./pengurus"));
 router.use("/controller", require("./pertemuan"));
 router.use("/controller", require("./reservasi"));
-router.use("/controller", require("./santri"));
-router.use("/controller", require("./shift"));
 router.use("/controller", require("./wilayah"));
 
 module.exports = router;
