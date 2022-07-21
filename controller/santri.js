343490c0d04fb52703077393a513307572c5a203
Alfad Sabil Haq <alfadsabilhaq@gmail.com>
2022-07-22 02:48:35 +0700

    Santri

--- controller/santri.js
+++ controller/santri.js
@@ -1,57 +1,72 @@
 const express = require("express");
 const router = express.Router();
+const Validator = require("fastest-validator");
+const v = new Validator();
 const database = require("../config/database");
 
 router.post("/", async (req, res) => {
-  const data = req.body;
-  const input = {
-    ...data,
+  const schema = {
+    uid_santri: { type: "string", min: 3, max: 255 },
+    nis: { type: "string", min: 3, max: 255 },
+    id_wilayah: { type: "number" },
+    id_lembaga: { type: "number" },
+    nama: { type: "string", min: 3, max: 255 },
+    jenkel: { type: "string" },
+    alamat: { type: "string", min: 3, max: 255 },
   };
+
+  const check = v.compile(schema);
+
+  const data = check(req.body);
   try {
-    const simpan = await database("santri").insert(input);
+    const simpan = await database("santri").insert(req.body);
     if (simpan) {
-      return res.status(200).json({
-        status: 1,
+      return res.status(201).json({
+        success: true,
         message: "Success",
-        result: {
-          id_santri: simpan[0],
-          ...input,
-        },
+        data: await database("santri").where("id_santri", simpan[0]).first(),
       });
     } else {
       return res.status(400).json({
-        status: 0,
+        success: false,
         message: "Failed",
       });
     }
   } catch (error) {
-    return res.status(500).json({
-      status: 0,
-      message: error.message,
+    return res.status(422).json({
+      success: false,
+      message: "Terdapat yang tidak sesuai",
+      error: error.sqlMessage,
+      data: data,
     });
   }
 });
 
-router.get("/santri/all", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const result = await database("santri").select("*");
+    const result = await database("santri")
+      .join("wilayah", "wilayah.id_wilayah", "=", "santri.id_wilayah")
+      .join("lembaga", "lembaga.id_lembaga", "=", "santri.id_lembaga")
+      .select("santri.*", "wilayah.nama_wilayah", "lembaga.nama_lembaga");
     return res.status(200).json({
       status: 1,
       message: "Success",
       result: result,
     });
   } catch (error) {
-    return res.status(500).json({
+    return res.status(400).json({
       status: 0,
       message: error.message,
     });
   }
 });
 
-router.get("/santri/one/:id_santri", async (req, res) => {
+router.get("/:id_santri", async (req, res) => {
   try {
     const result = await database("santri")
-      .select("*")
+      .join("wilayah", "wilayah.id_wilayah", "=", "santri.id_wilayah")
+      .join("lembaga", "lembaga.id_lembaga", "=", "santri.id_lembaga")
+      .select("santri.*", "wilayah.nama_wilayah", "lembaga.nama_lembaga")
       .where("id_santri", req.params.id_santri)
       .first();
     if (result) {
@@ -74,31 +89,57 @@ router.get("/santri/one/:id_santri", async (req, res) => {
   }
 });
 
-router.put("/santri/edit/:id_santri", async (req, res) => {
-  const data = req.body;
+router.put("/:id_santri", async (req, res) => {
+  const schema = {
+    uid_santri: { type: "string", min: 3, max: 255 },
+    nis: { type: "string", min: 3, max: 255 },
+    id_wilayah: { type: "number" },
+    id_lembaga: { type: "number" },
+    nama: { type: "string", min: 3, max: 255 },
+    jenkel: { type: "string" },
+    alamat: { type: "string", min: 3, max: 255 },
+  };
+
+  const check = v.compile(schema);
+
+  const data = check(req.body);
   try {
-    const result = await database("santri")
-      .where("id_santri", req.params.id_santri)
-      .first();
-    if (result) {
-      await database("santri")
-        .update(data)
-        .where("id_santri", req.params.id_santri);
-      return res.status(200).json({
-        status: 1,
-        message: "Success",
-        result: result,
+    const check = await database("santri")
+      .where("uid_santri", req.body.uid_santri)
+      .where("nis", req.body.nis)
+      .where("id_wilayah", req.body.id_wilayah)
+      .where("id_lembaga", req.body.id_lembaga)
+      .where("nama", req.body.nama)
+      .where("jenkel", req.body.jenkel)
+      .where("id_santri", "!=", req.params.id_santri);
+
+    if (check.length > 0) {
+      return res.status(422).json({
+        success: false,
+        message: "Data sudah ada",
+      });
+    }
+
+    const simpan = await database("santri")
+      .update(req.body)
+      .where("id_santri", req.params.id_santri);
+    if (simpan) {
+      return res.status(201).json({
+        success: true,
+        message: "Berhasil diubah",
       });
     } else {
       return res.status(400).json({
-        status: 0,
-        message: "Data not found",
+        success: false,
+        message: res.message,
       });
     }
   } catch (error) {
-    return res.status(500).json({
-      status: 0,
-      message: error.message,
+    return res.status(422).json({
+      success: false,
+      message: "Terdapat yang tidak sesuai",
+      error: error.sqlMessage,
+      data: data,
     });
   }
 });
